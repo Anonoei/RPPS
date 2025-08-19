f33d79fc92b992510112ecea956dc5b09c79d1bc
Anonoei <to+dev@an0.cx>
2025-08-18 17:02:30 -1000

    Add more comments and update serial.formats

--- src/rpps/serial/formats.py
+++ src/rpps/serial/formats.py
@@ -1,80 +1,45 @@
 """File format helpers"""
-import os
-import numpy as np
-from abc import ABC, abstractmethod
-
 from enum import Enum
 
+import numpy as np
+
 def from_i8(samples):
+    """Returns samples as int8"""
     samps = np.array(samples, dtype=np.int8)
     return samps.astype(np.float32).view(dtype=np.complex64)
 
 def from_i16(samples):
+    """Returns sampkes as int16"""
     samps = np.array(samples, dtype=np.int16)
     return samps.astype(np.float32).view(dtype=np.complex64)
 
 def from_f16(samples):
+    """Returns samples as float16"""
     samps = np.array(samples, dtype=np.float16)
     return samps.astype(np.float32).view(dtype=np.complex64)
 
 def from_f32(samples):
+    """Returns samples as float32"""
     samps = np.array(samples, dtype=np.float32)
     return samps.view(dtype=np.complex64)
 
-class Formats:
+def from_f64(samples):
+    """Returns samples as float32"""
+    samps = np.array(samples, dtype=np.float64)
+    return samps.view(dtype=np.complex128)
+
+class Formats(Enum):
+    """Generic data formats"""
     i8  = (2, from_i8)
     i16 = (4, from_i16)
     f16 = (4, from_f16)
     f32 = (8, from_f32)
+    f64 = (16, from_f64)
 
     def bytes(self):
+        """Returns how many bytes each complex value takes"""
         return self.value[0]
 
-    def read(self, path, count, offset):
-        return self.value[1](path, count, offset)
-
-class Format:
-    """File Format parent class"""
-    byte_count = 0
-
-    def __init__(self):
-        self.cur_samp = 0
-        self.max_samp = -1
-        self._last_path = None
-
-    def __str__(self):
-        return str(type(self).__name__)
-
-    def init(self, path, max_samp=-1):
-        """Initialize the format"""
-        print(f"Initializing with path: {path}")
-        if not self._last_path == path:
-            self._last_path = path
-        if max_samp == -1:
-            self.max_samp = os.path.getsize(path) // self.byte_count
-        self.cur_samp = 0
-
-    def read(self, count: int, path: str=None, offset: int=-1, skip=1):
-        """Read next block from file"""
-        if path is None and self._last_path is None:
-            raise Exception("No path provided!")
-
-        if path is not None:
-            if self._last_path is None or not self._last_path == path:
-                self.init(path)
-
-        if offset == -1:
-            offset = self.cur_samp
-
-        # samps = self._read(path=path, count=count, offset=offset)
-
-        for cur_ittr in range(0, self.max_samp):
-            print(f"Reading {path} using count {count}, offset {offset}")
-            yield self._read(path=path, count=count, offset=offset)
-            self.cur_samp += count * skip
-            offset = offset + (count * skip)
-
-    @staticmethod
-    @abstractmethod
-    def _read(path: str, count: int, offset: int):
-        pass
+    def read(self, samples):
+        """Convert samples to format"""
+        return self.value[1](samples)
--- src/rpps/utils/psd.py
+++ src/rpps/utils/psd.py
@@ -2,6 +2,7 @@ import numpy as np
 from ..filters.impl import gaussian
 
 def psd(samps, Fs: float = 1, vbw_hz=None):
+    """Perform Power Spectral Density"""
     y = np.abs(np.fft.fft(samps))**2 / (len(samps)*Fs)
     y = np.fft.fftshift(10.0*np.log10(y))
     if vbw_hz is not None:
@@ -10,7 +11,9 @@ def psd(samps, Fs: float = 1, vbw_hz=None):
     return y
 
 def vbw_calc(N: int, Fs: float, vbw_hz: float):
+    """Convert Hz to gaussian sigma"""
     return vbw_hz/(Fs / N)
 
 def vbw(samps, smooth: float):
+    """Perform gaussian smoothing"""
     return gaussian.gaussian(samps, gaussian.fwhm2sigma(smooth))
